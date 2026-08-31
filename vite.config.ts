e7b1885b8dc0d3d2a2872f8bf1bd329f248c295d
Alex Dombroski <alexdombroski1@gmail.com>
2026-08-30 18:46:53 -0600

    feat: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
+import { defineConfig } from "vite";
+import deno from "@deno/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno()],
-  base: '/join-tool',
-  publicDir: "/public",
-  optimizeDeps: {
-    exclude: ["./serverless/**"], // exclude from dependency pre-bundling
-  },
-  build: {
-    rollupOptions: {
-      input: "index.html", // your frontend entry point
-      external: ["./serverless/**"], // exclude serverless files from build
-    },
-  },
-})
+  base: "/join-tool/",
+});
